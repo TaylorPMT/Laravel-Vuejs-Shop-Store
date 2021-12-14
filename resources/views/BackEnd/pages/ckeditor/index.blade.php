404a70a475e41d6f687810e823da666993e1d03f
TimoCBA <timo@cybridge.jp>
2021-12-14 19:34:22 +0900

    add file config

--- resources/views/BackEnd/pages/ckeditor/index.blade.php
+++ resources/views/BackEnd/pages/ckeditor/index.blade.php
@@ -3,11 +3,10 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Ckeditor Demo</title>
+    <title>Upload File</title>
 </head>
 <body>
 <div class="container">
-    <div class="" style="margin-bottom: 50px; margin-top: 20px">Ckeditor Demo</div>
     <div class="row" id="app" >
         <ckeditor></ckeditor>
     </div>
