45be8cc9eda34ae0ab79a4623275e69ae059e6c0
TimoCBA <timo@cybridge.jp>
2022-01-04 19:16:07 +0900

    update

---
+++ package/CMS/Page/src/Providers/PageProviders.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace CMS\Page\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use App\Helpers\Helpers;
+
+class PageProviders extends ServiceProvider
+{
+    use Helpers;
+
+
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__ . "/../../routes/web.php");
+        $this->loadRoutesFrom(__DIR__ . "/../../routes/api.php");
+        $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'page');
+    }
+
+    public function register()
+    {
+        $configs = $this->split_files_with_basename($this->app['files']->glob(__DIR__ . '/../../config/*.php'));
+        foreach ($configs as $key => $row) {
+            $this->mergeConfigFrom($row, $key);
+        }
+        $repository = [
+            [
+                'interface' => NewRepositoryInterface::class,
+                'binding'   => NewRepository::class,
+            ],
+        ];
+        foreach ($repository as $bind) {
+            $this->app->bind($bind['interface'], $bind['binding']);
+        }
+    }
+}
