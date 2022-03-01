02a235e4a3fbe3709fd5b18ac756cdaa3203bf78
TimoCBA <timo@cybridge.jp>
2022-03-01 23:31:35 +0900

    u

--- resources/views/Frontend/fe-master.blade.php
+++ resources/views/Frontend/fe-master.blade.php
@@ -162,6 +162,8 @@
             "{{ asset('frontend/assets/js/main.min.js') }}",
         ], function() {});
     </script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
     <script>
         $(document).ready(function(){
 
