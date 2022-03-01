4e239b37c287291594dbdfc00509f25e5a6e67e9
TimoCBA <timo@cybridge.jp>
2022-03-02 00:16:43 +0900

    u

--- resources/views/Frontend/fe-master.blade.php
+++ resources/views/Frontend/fe-master.blade.php
@@ -16,6 +16,7 @@
     <script>
         TOKEN = "{{ session('token') }}"
     </script>
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
 
 </head>
 
@@ -177,7 +178,7 @@
         }
         $.ajaxSetup({
             headers: {
-                'X-CSRF-TOKEN': "{{ csrf_token() }}"
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
             }
         });
         function showPosition(position) {
